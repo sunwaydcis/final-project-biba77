e1d9dd3c98a0253f8522e6cbaf0fea8c98f14120
Habiba Tarek Abdalla Mohamed Hassouna <habibahassouna74@gmail.com>
2025-01-09 22:10:48 +0200

    Defined the gravity

--- src/main/scala-3/MainApp.scala
+++ src/main/scala-3/MainApp.scala
@@ -45,8 +45,8 @@ object MainApp extends JFXApp3{
           fill = Color.Brown
         }
         platforms += new Rectangle {
-          x = 250
-          y = 180
+          x = 200
+          y = 220
           width = 200
           height = 40
           fill = Color.Brown
@@ -61,26 +61,77 @@ object MainApp extends JFXApp3{
           x = 700
           y = 20
         }
+
         //Scene content
         content = ball +: timerText +: platforms
+
+        //Gravity and Physics Variables
+        var velocityY = 0.0
+        val gravity = 0.5
+        var onPlatform = false
 
         // Handle Keyboard input for ball movement
         onKeyPressed = keyEvent => {
           keyEvent.code match {
-            case KeyCode.Up => ball.centerY.value -= 10 // Move up
-            case KeyCode.Down => ball.centerY.value += 10 // Move down
-            case KeyCode.Left => ball.centerX.value -= 10 // Move left
-            case KeyCode.Right => ball.centerX.value += 10 // Move right
+            case KeyCode.Left => ball.centerX.value -= 20 // Move left
+            case KeyCode.Right => ball.centerX.value += 20 // Move right
+            case KeyCode.Up =>
+              if (onPlatform) {
+                velocityY = -10
+                onPlatform = false
+              }
             case _ => // Do nothing for other keys
           }
         }
 
-          //Timer Update
-          AnimationTimer { _ =>
-            val elapsedTime = (System.nanoTime() - startTime) / 1e9
-            timerText.text = f"Time:$elapsedTime%.1f"
-          }.start()
+        //Game Physics and Collisions detected
+        val gameLoop = AnimationTimer { _ =>
+          //Applying Gravity
+          velocityY += gravity
+          ball.centerY.value += velocityY
+
+          //Collision detection with platforms
+          onPlatform = false
+          platforms.foreach { platform =>
+            //Top collision
+            if (ball.centerY.value + ball.radius.value >= platform.y.value &&
+                ball.centerY.value <= platform.y.value + platform.height.value &&
+                ball.centerX.value >= platform.x.value &&
+                ball.centerX.value <= platform.x.value + platform.width.value) {
+              ball.centerY.value = platform.y.value - ball.radius.value //Places the ball on top of the platform
+              velocityY = 0 //Stops the downward motion
+              onPlatform = true
+            }
+
+            //Side Collisions
+            if (ball.centerX.value + ball.radius.value >= platform.x.value &&
+                ball.centerX.value - ball.radius.value <= platform.x.value + platform.width.value &&
+                ball.centerY.value >= platform.y.value &&
+                ball.centerY.value <= platform.y.value + platform.height.value) {
+              if (ball.centerX.value < platform.x.value) { //Left side
+                ball.centerX.value = platform.x.value - ball.radius.value
+              } else if (ball.centerX.value > platform.x.value + platform.width.value) {
+                ball.centerX.value = platform.x.value + platform.width.value + ball.radius.value
+              }
+            }
+          }
+
+          // Prevent ball from falling below the ground/platform
+          if (ball.centerY.value + ball.radius.value > 600) {
+            ball.centerY.value = 600 - ball.radius.value
+            velocityY = 0
+            onPlatform = true
+          }
+
+
+          // Timer Update
+          val elapsedTime = (System.nanoTime() - startTime) / 1e9
+          timerText.text = f"Time: $elapsedTime%.1f"
         }
+
+        // Start the AnimationTimer
+        gameLoop.start()
       }
     }
   }
+}
